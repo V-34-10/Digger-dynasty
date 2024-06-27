682f690754d6d6b6fbb924475aba38f6f0971249
Vlad <vladandryschenko98@gmail.com>
2024-06-28 00:36:36 +0300

    PrivacyActivity: screenOrientation landscape

---
+++ app/src/main/java/com/diggingdy/nastyslo/ui/privacy/PrivacyActivity.kt
@@ -0,0 +1,12 @@
+package com.diggingdy.nastyslo.ui.privacy
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.diggingdy.nastyslo.R
+
+class PrivacyActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_privacy)
+    }
+}
