0bf0e0c053764b60ec47a07400317f9fb23a3af9
Vlad <vladandryschenko98@gmail.com>
2024-06-28 00:34:35 +0300

    add package ui

--- app/src/main/java/com/diggingdy/nastyslo/ui/menu/MainActivity.kt
+++ app/src/main/java/com/diggingdy/nastyslo/ui/menu/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.diggingdy.nastyslo
+package com.diggingdy.nastyslo.ui.menu
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.diggingdy.nastyslo.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
