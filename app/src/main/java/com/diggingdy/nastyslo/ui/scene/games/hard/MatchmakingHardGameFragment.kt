bed1b3ce20b6e6fb85a736f45b1ae0384080f9bb
Vlad <vladandryschenko98@gmail.com>
2024-06-28 19:26:32 +0300

    UI FragmentGameMaria: add status_wins, control_bar_game, recycler scene_game

---
+++ app/src/main/java/com/diggingdy/nastyslo/ui/scene/games/hard/MatchmakingHardGameFragment.kt
@@ -0,0 +1,17 @@
+package com.diggingdy.nastyslo.ui.scene.games.hard
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.diggingdy.nastyslo.R
+
+class MatchmakingHardGameFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_matchmaking_hard_game, container, false)
+    }
+}
