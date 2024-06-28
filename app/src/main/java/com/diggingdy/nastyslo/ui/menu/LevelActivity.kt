f21a860d9d176b92a780dc7d7d9255f463f820f9
Vlad <vladandryschenko98@gmail.com>
2024-06-28 12:54:49 +0300

    UI LevelActivity: add textview, btn back, choose select level

---
+++ app/src/main/java/com/diggingdy/nastyslo/ui/menu/LevelActivity.kt
@@ -0,0 +1,12 @@
+package com.diggingdy.nastyslo.ui.menu
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.diggingdy.nastyslo.R
+
+class LevelActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_level)
+    }
+}
