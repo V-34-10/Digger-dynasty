78b54c9cbb720d22cf3e2f5c3e8199ed963281db
Vlad <vladandryschenko98@gmail.com>
2024-06-28 14:13:39 +0300

    UI SettingsActivity: add textview, btn back, panel sounds|vibration|level|theme

---
+++ app/src/main/java/com/diggingdy/nastyslo/ui/settings/SettingsActivity.kt
@@ -0,0 +1,12 @@
+package com.diggingdy.nastyslo.ui.settings
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.diggingdy.nastyslo.R
+
+class SettingsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+    }
+}
