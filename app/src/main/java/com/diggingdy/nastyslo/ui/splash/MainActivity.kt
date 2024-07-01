ac37a6f89d416fb81d91a2dc0abf24675f5b1b88
Vlad <vladandryschenko98@gmail.com>
2024-07-01 17:41:49 +0300

    ActivityMain: logic -> startPrivacy else -> MenuActivity

--- app/src/main/java/com/diggingdy/nastyslo/ui/splash/MainActivity.kt
+++ app/src/main/java/com/diggingdy/nastyslo/ui/splash/MainActivity.kt
@@ -1,12 +1,51 @@
 package com.diggingdy.nastyslo.ui.splash
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import com.diggingdy.nastyslo.HideNavigation
 import com.diggingdy.nastyslo.R
+import com.diggingdy.nastyslo.ui.menu.MenuActivity
+import com.diggingdy.nastyslo.ui.privacy.PrivacyActivity
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var sharedPref: SharedPreferences
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        HideNavigation.hideNavigation(this)
+        startPrivacy()
+    }
+
+    private fun startPrivacy() {
+        sharedPref = getSharedPreferences("diggingDynastyPref", MODE_PRIVATE)
+        val flagPrivacy = sharedPref.getBoolean("statusPrivacy", false)
+
+        if (!flagPrivacy) {
+            sharedPref.edit().putBoolean("statusPrivacy", true).apply()
+        }
+
+        lifecycleScope.launch {
+            delay(3000L)
+
+            val startIntent = if (!flagPrivacy) {
+                Intent(this@MainActivity, PrivacyActivity::class.java)
+            } else {
+                Intent(this@MainActivity, MenuActivity::class.java)
+            }
+
+            startActivity(startIntent)
+            finish()
+        }
+    }
+
+    @Deprecated("Deprecated in Java")
+    override fun onBackPressed() {
+        super.onBackPressed()
+        finish()
     }
 }
