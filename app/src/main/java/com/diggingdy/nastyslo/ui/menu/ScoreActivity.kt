d3e0734e06b639f938f228c8035ad3e36050f807
Vlad <vladandryschenko98@gmail.com>
2024-07-01 21:00:13 +0300

    ActivityScore: add confirmGameSettingsButton

--- app/src/main/java/com/diggingdy/nastyslo/ui/menu/ScoreActivity.kt
+++ app/src/main/java/com/diggingdy/nastyslo/ui/menu/ScoreActivity.kt
@@ -1,12 +1,38 @@
 package com.diggingdy.nastyslo.ui.menu
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.view.animation.AnimationUtils
+import androidx.appcompat.app.AppCompatActivity
+import com.diggingdy.nastyslo.HideNavigation
 import com.diggingdy.nastyslo.R
+import com.diggingdy.nastyslo.databinding.ActivityScoreBinding
+import com.diggingdy.nastyslo.ui.settings.SettingsActivity
 
 class ScoreActivity : AppCompatActivity() {
+    private val binding by lazy { ActivityScoreBinding.inflate(layoutInflater) }
+    private lateinit var sharedPref: SharedPreferences
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_score)
+        setContentView(binding.root)
+        HideNavigation.hideNavigation(this)
+        confirmGameSettingsButton()
+    }
+
+    private fun confirmGameSettingsButton() {
+        var animation = AnimationUtils.loadAnimation(this, R.anim.scale)
+        binding.btnOk.setOnClickListener {
+            it.startAnimation(animation)
+            startActivity(Intent(this@ScoreActivity, SettingsActivity::class.java))
+            finish()
+        }
+        binding.btnBack.setOnClickListener {
+            animation = AnimationUtils.loadAnimation(this, R.anim.scale)
+            it.startAnimation(animation)
+            sharedPref.edit().putString("levelGame", "").apply()
+            startActivity(Intent(this@ScoreActivity, LevelActivity::class.java))
+            finish()
+        }
     }
 }
