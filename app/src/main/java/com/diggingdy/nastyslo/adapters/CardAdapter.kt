d9df8050925b3fb0166d59b354ac8baff1824975
Vlad <vladandryschenko98@gmail.com>
2024-07-04 00:26:45 +0300

    CardAdapter: add spanCount, calculate cardWidth

--- app/src/main/java/com/diggingdy/nastyslo/adapters/CardAdapter.kt
+++ app/src/main/java/com/diggingdy/nastyslo/adapters/CardAdapter.kt
@@ -8,18 +8,24 @@ import androidx.recyclerview.widget.RecyclerView
 import com.diggingdy.nastyslo.R
 import com.diggingdy.nastyslo.model.Card
 
-class CardAdapter(private val cardList: List<Card>) :
+class CardAdapter(private val cardList: List<Card>, private val spanCount: Int) :
     RecyclerView.Adapter<CardAdapter.CardViewHolder>() {
 
-    private var onCardClick: ((Card, Int) -> Unit)? = null
+    var onCardClick: ((Card, Int) -> Unit)? = null
 
     class CardViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val cardImage: ImageView = itemView.findViewById(R.id.cardImage)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CardViewHolder {
-        val itemView = LayoutInflater.from(parent.context)
-            .inflate(R.layout.card_image, parent, false)
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val itemView = layoutInflater.inflate(R.layout.card_image, parent, false)
+
+        val displayMetrics = parent.context.resources.displayMetrics
+        val screenWidth = displayMetrics.widthPixels
+        val cardWidth = screenWidth / spanCount
+
+        itemView.layoutParams = ViewGroup.LayoutParams(cardWidth, cardWidth)
         return CardViewHolder(itemView)
     }
 
