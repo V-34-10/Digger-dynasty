8a2442718431a8e950ef0a5c1810d85b061f5c58
Vlad <vladandryschenko98@gmail.com>
2024-07-04 22:29:23 +0300

    GameMysticMaria: add|use ControllerGame, optimization logicGame, soundTheme

--- app/src/main/java/com/diggingdy/nastyslo/ui/scene/games/MatchmakingHardGameFragment.kt
+++ app/src/main/java/com/diggingdy/nastyslo/ui/scene/games/MatchmakingHardGameFragment.kt
@@ -1,17 +1,90 @@
 package com.diggingdy.nastyslo.ui.scene.games
 
+import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AnimationUtils
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import com.diggingdy.nastyslo.R
+import com.diggingdy.nastyslo.databinding.FragmentMatchmakingHardGameBinding
+import com.diggingdy.nastyslo.ui.menu.LevelActivity
+import com.diggingdy.nastyslo.ui.menu.ScoreActivity
+import com.diggingdy.nastyslo.ui.settings.SoundManager
 
 class MatchmakingHardGameFragment : Fragment() {
+    private lateinit var binding: FragmentMatchmakingHardGameBinding
+    private lateinit var sharedPref: SharedPreferences
+    private var selectedLevel: String = ""
+    private lateinit var soundManager: SoundManager
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_matchmaking_hard_game, container, false)
+    ): View {
+        binding = FragmentMatchmakingHardGameBinding.inflate(layoutInflater, container, false)
+
+        soundManager = context?.let { SoundManager(it) }!!
+        sharedPref =
+            requireActivity().getSharedPreferences(
+                "diggingDynastyPref",
+                AppCompatActivity.MODE_PRIVATE
+            )
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        soundMode()
+        setSelectedLevel()
+        context?.let { ControllerGame.initRecyclerSceneGame(binding, it) }
+        controlGameButton()
+    }
+
+    private fun setSelectedLevel() {
+        selectedLevel = sharedPref.getString("levelGame", "").toString()
+        binding.textSelectedLevel.text = selectedLevel
+    }
+
+    private fun controlGameButton() {
+        var animation = AnimationUtils.loadAnimation(context, R.anim.scale)
+        binding.btnStart.setOnClickListener {
+            it.startAnimation(animation)
+            ControllerGame.resetGame()
+        }
+        binding.btnChange.setOnClickListener {
+            animation = AnimationUtils.loadAnimation(context, R.anim.scale)
+            it.startAnimation(animation)
+            startActivity(Intent(context, LevelActivity::class.java))
+            activity?.finish()
+        }
+        binding.btnHighScore.setOnClickListener {
+            animation = AnimationUtils.loadAnimation(context, R.anim.scale)
+            it.startAnimation(animation)
+            startActivity(Intent(context, ScoreActivity::class.java))
+            activity?.finish()
+        }
+        binding.btnBack.setOnClickListener {
+            animation = AnimationUtils.loadAnimation(context, R.anim.scale)
+            it.startAnimation(animation)
+            activity?.onBackPressed()
+        }
+    }
+
+    private fun soundMode() {
+        val isSound = sharedPref.getBoolean("sound_enabled", false)
+        if (isSound) {
+            soundManager.apply {
+                playSound(R.raw.sound_mystic_maria, true)
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        soundManager.release()
     }
 }
