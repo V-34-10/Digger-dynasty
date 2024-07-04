5ae2f2cebee401f0a5d5728aba2af3cde7d30008
Vlad <vladandryschenko98@gmail.com>
2024-07-04 17:52:34 +0300

    Model Card: add position -> unique cards

--- app/src/main/java/com/diggingdy/nastyslo/model/Card.kt
+++ app/src/main/java/com/diggingdy/nastyslo/model/Card.kt
@@ -3,5 +3,6 @@ package com.diggingdy.nastyslo.model
 data class Card(
     val imageResId: Int,
     var isFlipped: Boolean = false,
-    var isMatched: Boolean = false
+    var isMatched: Boolean = false,
+    var position: Int = -1
 )
